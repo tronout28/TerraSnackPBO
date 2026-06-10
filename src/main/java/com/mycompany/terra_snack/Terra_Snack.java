a5ee6c177764156bba271b0c06570c958e2b8409
tronout28 <fatihabdurahman28@gmail.com>
2026-06-10 19:42:28 +0700

    initial commit

---
+++ src/main/java/com/mycompany/terra_snack/Terra_Snack.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.terra_snack;
+
+/**
+ *
+ * @author ftih_28
+ */
+public class Terra_Snack {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
