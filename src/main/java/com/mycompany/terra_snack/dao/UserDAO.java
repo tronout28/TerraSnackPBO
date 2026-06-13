c25a93646197c2411bb5056887a0b3cfc4217241
tronout28 <fatihabdurahman28@gmail.com>
2026-06-13 13:33:37 +0700

    create and add Login form and auth Logic [FA]

--- src/main/java/com/mycompany/terra_snack/dao/UserDAO.java
+++ src/main/java/com/mycompany/terra_snack/dao/UserDAO.java
@@ -3,11 +3,167 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package com.mycompany.terra_snack.dao;
+import com.mycompany.terra_snack.database.DatabaseConnection;
+import com.mycompany.terra_snack.model.User;
 
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 /**
  *
  * @author ftih_28
  */
 public class UserDAO {
     
+   
+    public boolean insert(User user) {
+
+        String sql =
+                "INSERT INTO users "
+                + "(nama, username, password, role, no_hp) "
+                + "VALUES (?, ?, ?, ?, ?)";
+
+        try (Connection conn =
+                     DatabaseConnection.getConnection();
+             PreparedStatement ps =
+                     conn.prepareStatement(sql)) {
+
+            ps.setString(1, user.getNama());
+            ps.setString(2, user.getUsername());
+            ps.setString(3, user.getPassword());
+            ps.setString(4, user.getRole());
+            ps.setString(5, user.getNoHp());
+
+            return ps.executeUpdate() > 0;
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return false;
+    }
+
+
+    public List<User> getAllUsers() {
+
+        List<User> users = new ArrayList<>();
+
+        String sql = "SELECT * FROM users";
+
+        try (Connection conn =
+                     DatabaseConnection.getConnection();
+             Statement st =
+                     conn.createStatement();
+             ResultSet rs =
+                     st.executeQuery(sql)) {
+
+            while (rs.next()) {
+
+                User user = new User();
+
+                user.setUserId(rs.getInt("user_id"));
+                user.setNama(rs.getString("nama"));
+                user.setUsername(rs.getString("username"));
+                user.setPassword(rs.getString("password"));
+                user.setRole(rs.getString("role"));
+                user.setNoHp(rs.getString("no_hp"));
+
+                users.add(user);
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return users;
+    }
+
+
+    public User getUserById(int id) {
+
+        String sql =
+                "SELECT * FROM users WHERE user_id=?";
+
+        try (Connection conn =
+                     DatabaseConnection.getConnection();
+             PreparedStatement ps =
+                     conn.prepareStatement(sql)) {
+
+            ps.setInt(1, id);
+
+            ResultSet rs = ps.executeQuery();
+
+            if (rs.next()) {
+
+                User user = new User();
+
+                user.setUserId(rs.getInt("user_id"));
+                user.setNama(rs.getString("nama"));
+                user.setUsername(rs.getString("username"));
+                user.setPassword(rs.getString("password"));
+                user.setRole(rs.getString("role"));
+                user.setNoHp(rs.getString("no_hp"));
+
+                return user;
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+
+    public boolean update(User user) {
+
+        String sql =
+                "UPDATE users SET "
+                + "nama=?, "
+                + "username=?, "
+                + "password=?, "
+                + "role=?, "
+                + "no_hp=? "
+                + "WHERE user_id=?";
+
+        try (Connection conn =
+                     DatabaseConnection.getConnection();
+             PreparedStatement ps =
+                     conn.prepareStatement(sql)) {
+
+            ps.setString(1, user.getNama());
+            ps.setString(2, user.getUsername());
+            ps.setString(3, user.getPassword());
+            ps.setString(4, user.getRole());
+            ps.setString(5, user.getNoHp());
+            ps.setInt(6, user.getUserId());
+
+            return ps.executeUpdate() > 0;
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return false;
+    }
+
+    public boolean delete(int id) {
+
+        String sql =
+                "DELETE FROM users WHERE user_id=?";
+
+        try (Connection conn =
+                     DatabaseConnection.getConnection();
+             PreparedStatement ps =
+                     conn.prepareStatement(sql)) {
+
+            ps.setInt(1, id);
+
+            return ps.executeUpdate() > 0;
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return false;
+    }
 }
--- src/main/java/com/mycompany/terra_snack/services/AuthServices.java
+++ src/main/java/com/mycompany/terra_snack/services/AuthServices.java
@@ -4,10 +4,66 @@
  */
 package com.mycompany.terra_snack.services;
 
+import com.mycompany.terra_snack.database.DatabaseConnection;
+import com.mycompany.terra_snack.model.User;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 /**
  *
  * @author ftih_28
  */
 public class AuthServices {
+
+      public User login(String username,
+                      String password) {
+
+        String sql =
+                "SELECT * FROM users "
+                + "WHERE username=? "
+                + "AND password=?";
+
+        try (Connection conn =
+                     DatabaseConnection.getConnection();
+             PreparedStatement ps =
+                     conn.prepareStatement(sql)) {
+
+            ps.setString(1, username);
+            ps.setString(2, password);
+
+            ResultSet rs = ps.executeQuery();
+
+            if (rs.next()) {
+
+                User user = new User();
+
+                user.setUserId(
+                        rs.getInt("user_id"));
+
+                user.setNama(
+                        rs.getString("nama"));
+
+                user.setUsername(
+                        rs.getString("username"));
+
+                user.setPassword(
+                        rs.getString("password"));
+
+                user.setRole(
+                        rs.getString("role"));
+
+                user.setNoHp(
+                        rs.getString("no_hp"));
+
+                return user;
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return null;
+    }
     
 }
